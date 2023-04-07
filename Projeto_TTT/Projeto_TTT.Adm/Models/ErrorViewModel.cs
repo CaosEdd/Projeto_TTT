e2b536ba30b848acf09221f0a467abd2943825ea
Alexandre Rodrigues <btt.ale@gmail.com>
2023-04-07 17:30:46 -0300

    implementa estrutura de projeto

---
+++ Projeto_TTT/Projeto_TTT.Adm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projeto_TTT.Adm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
